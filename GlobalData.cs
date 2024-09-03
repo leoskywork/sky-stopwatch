9d8ce9e608ae366388fdc3b7fd422f33b63e952a
leo pc pro <leoskywork@outlook.com>
2024-09-04 01:54:33 +0800

    version 1.7.2

--- GlobalData.cs
+++ GlobalData.cs
@@ -12,7 +12,7 @@ namespace SkyStopwatch
     public class GlobalData
     {
         public const string Version = "1.7";
-        public const string Subversion = "1.240829";
+        public const string Subversion = "2.240904";//"1.240829";
         public const string ChangeTimeSourceOCRTimeIsNegativeOne = "ocrDisplayTime is -1";
         public const string ChangeTimeSourceClearButton = "OnClearOCR";
         public const string ChangeTimeSourceManualOCRButton = "ManualOCR";
